780eb6ce3fa1b3a0679dbf89cb707ab1b11a803b
Chandan Soni <90852214+Chandansoni2021@users.noreply.github.com>
2022-07-13 00:08:22 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -11,13 +11,10 @@ for(int i=0;i<n;i++){
 }
 printf("please enter the value which u search in array :-");
 scanf("%d",&value);
-for(i=0;i<n;i++){
-    if arr[i]==value{
-    printf("%d", i)
+for(int i=0;i<n;i++){
+    if(arr[i]==value){
+    printf("%d", i);
     }
-    else{
-        printf("this value is not present in this array");
     }
-}
 return 0;
 }
